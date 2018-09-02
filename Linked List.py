d8ab97b2c64d6b4652412a3be9e3960a9b824440
Mark <mjc862@mail.usask.ca>
2018-09-01 21:25:31 -0600

    Added Delete first, last and at index function

--- Linked List.py
+++ Linked List.py
@@ -36,6 +36,16 @@ class LinkedList(object):
             self._root = Node(value, self._root)
         self._count += 1
 
+    def delete_front(self):
+        """
+        Delete from the front of the list (delete index 0)
+        Pre-condition: List is not empty
+        """
+        if self.is_empty():
+            raise Exception("Linked List is Empty!")
+        self._root = self._root.next()
+        self._count -= 1
+
     def insert_last(self, value):
         """
         Insert a new node at the end of our list
@@ -49,6 +59,26 @@ class LinkedList(object):
             self._last = new_node
         self._count += 1
 
+    def delete_last(self):
+        """
+        Delete the item in the end of our linked list
+        Pre-condition: List is not empty
+        """
+        if self.is_empty():
+            raise Exception("Linked List is Empty!")
+        if self.length() == 1:
+            self._root = None
+            self._last = None
+        else:
+            prev_last = None
+            curr_node = self._root
+            while curr_node.has_next():
+                prev_last = curr_node
+                curr_node = curr_node.next()
+            prev_last.set_next(None)
+            self._last = prev_last
+        self._count -= 1
+
     def insert_at_index(self, index, value):
         """
         Insert a new value located at index 'index'
@@ -56,7 +86,7 @@ class LinkedList(object):
         :param value: value to be inserted in pos index
         """
         if self._count <= index:
-            raise Exception("Index cannot be greater than the size of our list!s")
+            raise Exception("Index cannot be greater than the size of our list!")
         if self.is_empty() or index == 0:
             self.insert_front(value)
         elif self.length() - 1 == index:
@@ -72,6 +102,28 @@ class LinkedList(object):
             prev.set_next(Node(value))
             prev.next().set_next(curr)
         self._count += 1
+
+    def delete_at_index(self, index):
+        """
+        delete the node at index n
+        :param index: index of the node to be deleted
+        """
+        if self.is_empty():
+            raise Exception("Linked List is empty!")
+        elif index >= self.length():
+            raise ValueError("Index out of Range!")
+        if index == 0:
+            self.delete_front()
+        elif index == self.length() - 1:
+            self.delete_last()
+        else:
+            current_node = self._root
+            prev_node = None
+            for i in range(index):
+                prev_node = current_node
+                current_node = current_node.next()
+            prev_node.set_next(current_node.next())
+        self._count -= 1
 
     def contains(self, value):
         """
@@ -137,7 +189,7 @@ if __name__ == "__main__":
     print("Current State: " + str(ll))
 
     for i in range(10):
-        ll.insert_front(i)
+        ll.insert_front(9-i)
     print("Current State after insertion to front: " + str(ll))
     current_index = 0
 
@@ -147,5 +199,13 @@ if __name__ == "__main__":
         current_index += 1
     if ll.length() != 10:
         print("Current length should be 10!")
+
+    print(ll)
+    ll.delete_at_index(8)
+    ll.delete_at_index(8)
+    ll.delete_at_index(4)
+    print(ll)
+
+
 
 
